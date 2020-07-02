227dfb28a34d8fa3067ffbbe324bba21a4dc9e11
cuj1971 <65290482+cuj1971@users.noreply.github.com>
2020-07-02 13:37:20 +0200

    added environment prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  newsConfig : {
+    apiKey: 'B2O7riG2DAbmoBannY9R03ytV2AiACpP',
+  },
+  firebaseConfig : {
+    apiKey: "AIzaSyAYVA762HUhravxp9AJdB93ACnEM4ZfMgY",
+    authDomain: "newsarchive-17092.firebaseapp.com",
+    databaseURL: "https://newsarchive-17092.firebaseio.com",
+    projectId: "newsarchive-17092",
+    storageBucket: "newsarchive-17092.appspot.com",
+    messagingSenderId: "277353383647",
+    appId: "1:277353383647:web:4fa3eae7e4703d86dbcb6a"
+  }
 };
